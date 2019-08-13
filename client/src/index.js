d3fbeec5174f61f3c4fffb295b704e1a954005e4
Sajid Ali <sajid@192.168.10.11>
2019-08-14 03:27:38 +0500

    completed redux integration

--- client/src/index.js
+++ client/src/index.js
@@ -1,20 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "./index.css";
-import Navigation from "../src/containers/Navigation";
-import Dashboard from "../src/containers/Dashboard";
+import { Provider } from 'react-redux';
 import Login from "../src/containers/Login";
 import Quote from "../src/containers/Quote";
+import Dashboard from "../src/containers/Dashboard";
+import Navigation from "../src/containers/Navigation";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+
+import "./index.css";
 import App from "./App";
 
+import { store } from './store'
+
 ReactDOM.render(
-  <Router>
-    <Navigation />
-    <Route exact path="/" component={Login} />
-    <Route exact path="/login" component={Login} />
-    <Route exact path="/dashboard" component={Dashboard} />
-    <Route exact path="/create-quote" component={Quote} />
-  </Router>,
+
+  <Provider store={store}>
+    <Router>
+      <Navigation />
+      <Route exact path="/" component={Login} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/dashboard" component={Dashboard} />
+      <Route exact path="/create-quote" component={Quote} />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
