5fae0a251115cecdbaf94452be37643ea4e89880
Sajid Ali <sajid@192.168.10.11>
2019-08-14 03:51:34 +0500

    integrated redux saga

---
+++ client/src/sagas/index.js
@@ -0,0 +1,17 @@
+import { call, put, takeLatest } from 'redux-saga/effects'
+// import Api from '...'
+
+function* fetchUser(action) {
+    try {
+        // const user = yield call(Api.fetchUser, action.payload.userId);
+        yield put({ type: "USER_FETCH_SUCCEEDED", user: {} });
+    } catch (e) {
+        yield put({ type: "USER_FETCH_FAILED", message: e.message });
+    }
+}
+
+function* mySaga() {
+    yield takeLatest("USER_FETCH_REQUESTED", fetchUser);
+}
+
+export default mySaga;
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { createStore, combineReducers } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
 import reducers from '../reducers/index';
+import mySaga from '../sagas/index';
+
+const sagaMiddleware = createSagaMiddleware()
 
 export const store = createStore(
     combineReducers({
         state: reducers
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    applyMiddleware(sagaMiddleware)
 );
+
+sagaMiddleware.run(mySaga)
