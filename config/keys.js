ff986c607fdc35c9d69c524c3ec2acfd579b08e5
Jamil Abbas <jamil.abbas813@gmail.com>
2019-08-15 14:45:52 +0500

    Added logon and current user apis

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb+srv://sajid:123pakistan@uprisesmartshades-xfxv2.mongodb.net/test?retryWrites=true&w=majority"
+    "mongodb+srv://sajid:123pakistan@uprisesmartshades-xfxv2.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
 };
--- server.js
+++ server.js
@@ -21,6 +21,6 @@ mongoose
   .then(() => console.log("Connected to db"))
   .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("Hello World!"));
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
